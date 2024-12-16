7ac62bf8ae937cd400042784bfe81bd7d7d90448
Matija Turcic <115079275+mturcic1@users.noreply.github.com>
2024-12-17 00:06:32 +0100

    Update json_len.py

--- Helper scripts/json_len.py
+++ Helper scripts/json_len.py
@@ -1,19 +1,16 @@
 import json
 
 # Full path to your JSON file
-file_path = "D:/cleaned_eng_survival_of_the_strongest.json"
+file_path = "your_path"
 
 
 def count_questions(file_path):
-    # Load the JSON data
     with open(file_path, "r", encoding="utf-8") as file:
         data = json.load(file)
 
-    # Count the number of questions
     total_questions = len(data)
 
-    # Print the count
     print(f"Total number of questions: {total_questions}")
 
-# Run the script
+
 count_questions(file_path)
