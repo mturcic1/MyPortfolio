b1e2f2a749c6603e07e22c46efe901c34e9d6f82
Matija Turcic <115079275+mturcic1@users.noreply.github.com>
2024-12-17 00:05:53 +0100

    Update json_flatener.py

--- Helper scripts/json_flatener.py
+++ Helper scripts/json_flatener.py
@@ -1,20 +1,17 @@
 import json
 
 # Input and output file paths
-input_file_path = "D:\\eng_survival_of_the_strongest.json"  # Path to your input JSON file
-output_file_path = "D:\\flattened_questions.json"  # Path for the output JSON file
+input_file_path = "your_path"  
+output_file_path = "your_path"  
 
-# Read the JSON file
 with open(input_file_path, "r", encoding="utf-8") as infile:
     data = json.load(infile)
 
-# Flatten the questions
 flattened_questions = {}
 for question in data["questions"]["questions"]:
     question_id = question.pop("question_id")  # Extract the question ID as the key
     flattened_questions[question_id] = question
 
-# Save the flattened JSON
 with open(output_file_path, "w", encoding="utf-8") as outfile:
     json.dump(flattened_questions, outfile, indent=4, ensure_ascii=False)
 
